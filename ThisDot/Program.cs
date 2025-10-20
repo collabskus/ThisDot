d331a08bf3a569e1150bc703922104ee0c0aa9df
kushal <collabskus@gmail.com>
2025-10-20 16:01:16 -0400

    initialize a new repository
    :set paste
    Windows PowerShell
    Copyright (C) Microsoft Corporation. All rights reserved.
    
    Install the latest PowerShell for new features and improvements! https://aka.ms/PSWindows
    
    PS C:\Users\kushal> cd C:\Users\kushal\source\repos\ThisDot\
    PS C:\Users\kushal\source\repos\ThisDot> dotnet clean
        info NETSDK1057: You are using a preview version of .NET. See: https://aka.ms/dotnet-support-policy
    
    Build succeeded in 0.4s
    PS C:\Users\kushal\source\repos\ThisDot> tree /f
    Folder PATH listing
    Volume serial number is 9892-5BAD
    C:.
    │   ThisDot.slnx
    │
    └───ThisDot
        │   Program.cs
        │   ThisDot.csproj
        │
        ├───bin
        │   └───Debug
        │       └───net10.0
        └───obj
            │   project.assets.json
            │   project.nuget.cache
            │   ThisDot.csproj.nuget.dgspec.json
            │   ThisDot.csproj.nuget.g.props
            │   ThisDot.csproj.nuget.g.targets
            │
            └───Debug
                └───net10.0
                    │   .NETCoreApp,Version=v10.0.AssemblyAttributes.cs
                    │   apphost.exe
                    │   ThisDot.assets.cache
                    │   ThisDot.csproj.BuildWithSkipAnalyzers
                    │   ThisDot.csproj.FileListAbsolute.txt
                    │   ThisDot.GlobalUsings.g.cs
                    │
                    ├───ref
                    └───refint
    PS C:\Users\kushal\source\repos\ThisDot> git status
    fatal: not a git repository (or any of the parent directories): .git
    PS C:\Users\kushal\source\repos\ThisDot> dotnet new gitignore
    The template "dotnet gitignore file" was created successfully.
    
    PS C:\Users\kushal\source\repos\ThisDot> git init
    Initialized empty Git repository in C:/Users/kushal/source/repos/ThisDot/.git/
    PS C:\Users\kushal\source\repos\ThisDot> git remote show
    PS C:\Users\kushal\source\repos\ThisDot> git remote add origin https://github.com/collabskus/ThisDot.git
    PS C:\Users\kushal\source\repos\ThisDot> git status
    On branch master
    
    No commits yet
    
    Untracked files:
      (use "git add <file>..." to include in what will be committed)
            .gitignore
            ThisDot.slnx
            ThisDot/
    
    nothing added to commit but untracked files present (use "git add" to track)
    PS C:\Users\kushal\source\repos\ThisDot> git add .
    PS C:\Users\kushal\source\repos\ThisDot> git commit
    Aborting commit due to empty commit message.
    PS C:\Users\kushal\source\repos\ThisDot>

---
+++ ThisDot/Program.cs
@@ -0,0 +1,31 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        string name = "Kushal";
+        Person person = new Person(name);
+        string greeting = person.Greet();
+        Console.WriteLine(greeting);
+    }
+}
+
+public class Person
+{
+    private string name;
+    public Person(string name)
+    {
+        // using name = name is incorrect here 
+        // because it is not clear which name we are talking about 
+        // so we use this dot here 
+        // name = name;	
+        this.name = name;
+    }
+    public string Greet()
+    {
+        return "Namaste " + name;
+    }
+}
+// Output: Namaste Kushal
+// Explanation: In this example, we have a Person class with a constructor that takes a name parameter.
+// Inside the constructor, we use this.name to refer to the instance variable name,
+// distinguishing it from the parameter name. This ensures that the instance variable is correctly assigned the value passed to the constructor.
